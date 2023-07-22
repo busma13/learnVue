db1051730c0e45feeb534086808e53cea36a5a7f
busma13 <peterluitjens@gmail.com>
2023-07-21 20:52:26 -0700

    conditionals: v-if v-else v-show

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const app = Vue.createApp({
   // template: "<h2>I am the template</h2>"
   data() {
     return {
+      showBooks: true,
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 45,
     };
   },
   methods: {
-    changeTitle(title) {
-      this.title = title;
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
+    // changeTitle(title) {
+    //   this.title = title;
+    // },
   },
 });
 
